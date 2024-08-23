72b99010f24be50e7618210bb7caecbf5c5e87f2
Nguyen Chi Cuong <20020331.cuong@student.iuh.edu.vn>
2024-08-23 11:09:07 +0700

    add  Tour_Destination

---
+++ src/main/java/vn/edu/iuh/fit/entity/TourDestinationId.java
@@ -0,0 +1,1 @@
+package vn.edu.iuh.fit.entity;import lombok.*;import java.io.Serializable;@AllArgsConstructor@NoArgsConstructor@EqualsAndHashCode@Getter@Setterpublic class TourDestinationId implements Serializable {    private Long tour;    private Long destination;}
