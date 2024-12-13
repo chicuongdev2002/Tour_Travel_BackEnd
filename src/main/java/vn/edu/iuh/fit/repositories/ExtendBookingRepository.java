cdf924f1b2ce8315e48e249aac22c3bda4f10fe0
Sơn Nguyễn <sonnt1@imt-soft.com>
2024-12-14 01:27:58 +0700

    fix condition update tour

--- src/main/java/vn/edu/iuh/fit/repositories/ExtendBookingRepository.java
+++ src/main/java/vn/edu/iuh/fit/repositories/ExtendBookingRepository.java
@@ -3,5 +3,8 @@ package vn.edu.iuh.fit.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.edu.iuh.fit.entity.ExtendBooking;
 
+import java.util.List;
+
 public interface ExtendBookingRepository extends JpaRepository<ExtendBooking, String> {
+    List<ExtendBooking> findAllByTourId(long tourId);
 }
